3dc623e992fb08b66de89574aefad5d236b38fe4
Bent Mittet Opdahl <BentsImac@bents-imac.lan>
2020-12-23 15:32:55 +0100

    fiksa build errors

--- src/components/pages/Informasjon.js
+++ src/components/pages/Informasjon.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../../App.css';
-import HeroSection from '../HeroSection';
+//import HeroSection from '../HeroSection';
 import Info from "../Info";
 import Footer from '../Footer';
 
