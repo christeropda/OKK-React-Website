93eba7758ff8d1a7019bdba9c94bf50e46833301
christer <christer.opda@gmail.com>
2020-12-23 22:30:11 +0100

    jul igjenn

--- src/components/pages/Informasjon.js
+++ src/components/pages/Informasjon.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import '../../App.css';
-//import HeroSection from '../HeroSection';
-import Info from "../Info";
 import Footer from '../Footer';
-import Cards from "../Cards"
+import Cards from "../Cards";
 
 function Home() {
     return(
